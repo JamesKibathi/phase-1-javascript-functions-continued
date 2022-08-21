76626ca45369194ea9de1bec2a34f20dcfd10859
James Njenga <knjenga94@gmail.com>
2022-08-21 03:16:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+
+function saturdayFun(what="roller-skate"){
+
+   let myDay= "This Saturday, I want to "+what+"!";
+
+   return myDay;
+}
+
+function mondayWork(toDo="go to the office"){
+    let monday="This Monday, I will "+toDo+".";
+    return monday;
+
+}
+
+  function wrapAdjective(word="special"){
+
+  const innerFunction = function(ad){
+    
+    return `You are ${word}${ad}${word}!`;
+
+    };
+
+   return innerFunction;
+}
+let ans=wrapAdjective("*")("a hard worker");
+wrapAdjective("||")("a dedicated programmer");
+console.log(ans);
+
+
